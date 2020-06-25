c01156f002019bdde6914511de7f6e5693473121
alyson-b69 <alyson.bernabeu@gmail.com>
2020-06-25 10:13:18 +0200

    add js function et html

--- app.js
+++ app.js
@@ -1,78 +1,118 @@
-const practicians=[
-    {
-        id:1,
-        clinic:'Corazon del Mundo',
-        city:'Asuncion',
-        country:'Paraguay',
-        continent:'Latina America',
-        img_clinique:'',
-        map_clinique:'',
-        name_practician:'Alyson',
-        img_practician:'',
-        speciality_practician:'Cardiology',
-        surgeries:['repair broken heart'],
-        description_clinic:'Welcome to Corazon del Mundo clinic. ',
-        package:[''],
-    },
-    {
-        id:2,
-        clinic:'Abidjan Center',
-        city:'Abidjan',
-        country:'Ivory Coast',
-        continent:'Africa',
-        img_clinique:'',
-        map_clinique:'',
-        name_practician:'Alexandra',
-        img_practician:'',
-        speciality_practician:'Cardiology',
-        operations:[''],
-        description_clinic:'Abidjan Center is the most famous medical center in Africa.',
-        package:[''],
-    },
-    {
-        id:3,
-        clinic:'Sun Clinic',
-        city:'London',
-        country:'England',
-        continent:'Europe',
-        img_clinique:'',
-        map_clinique:'',
-        name_practician:'Ismael',
-        img_practician:'',
-        speciality_practician:'Pediatric',
-        operations:['pneumology'],
-        description_clinic:'Every child is awesome',
-        package:[''],
-    },
-    {
-        id:4,
-        clinic:'The smiling clinic',
-        city:'Pataya',
-        country:'Thailand',
-        continent:'Asia',
-        img_clinique:'',
-        map_clinique:'',
-        name_practician:'Guillaume',
-        img_practician:'',
-        speciality_practician:'Plastic surgery',
-        operations:['Breast surgery'],
-        description_clinic:'See big',
-        package:[''],
-    },
-    {
-        id:5,
-        clinic:'Smart HQ',
-        city:'Melbourn',
-        country:'Australia',
-        continent:'Oceania',
-        img_clinique:'',
-        map_clinique:'',
-        name_practician:'Trad',
-        img_practician:'',
-        speciality_practician:'Neurology',
-        operations:['Implant chips'],
-        description_clinic:"Welcome to the brain's headquarter!",
-        package:[''],
-    }
+(function () {
+        
+     let practicians = [
+        {
+            id:1,
+            clinic:'Corazon del Mundo',
+            city:'Asuncion',
+            country:'Paraguay',
+            continent:'Latina America',
+            img_clinique:'https://www.lasource.ch/sites/default/files/styles/crop_3_2/public/images/content_types/page/specialites.jpg?itok=1eShRFbt',
+            map_clinique:'',
+            name_practician:'Alyson',
+            img_practician:'',
+            speciality_practician:'Cardiology',
+            surgeries:['repair broken heart'],
+            description_clinic:'Welcome to Corazon del Mundo clinic. ',
+            package:[''],
+        },
+        {
+            id:2,
+            clinic:'Abidjan Center',
+            city:'Abidjan',
+            country:'Ivory Coast',
+            continent:'Africa',
+            img_clinique:'https://www.lasource.ch/sites/default/files/styles/crop_3_2/public/images/content_types/page/specialites.jpg?itok=1eShRFbt',
+            map_clinique:'',
+            name_practician:'Alexandra',
+            img_practician:'',
+            speciality_practician:'Cardiology',
+            operations:[''],
+            description_clinic:'Abidjan Center is the most famous medical center in Africa.',
+            package:[''],
+        },
+        {
+            id:3,
+            clinic:'Sun Clinic',
+            city:'London',
+            country:'England',
+            continent:'Europe',
+            img_clinique:'https://www.lasource.ch/sites/default/files/styles/crop_3_2/public/images/content_types/page/specialites.jpg?itok=1eShRFbt',
+            map_clinique:'',
+            name_practician:'Ismael',
+            img_practician:'',
+            speciality_practician:'Pediatric',
+            operations:['pneumology'],
+            description_clinic:'Every child is awesome',
+            package:[''],
+        },
+        {
+            id:4,
+            clinic:'The smiling clinic',
+            city:'Pataya',
+            country:'Thailand',
+            continent:'Asia',
+            img_clinique:'https://www.lasource.ch/sites/default/files/styles/crop_3_2/public/images/content_types/page/specialites.jpg?itok=1eShRFbt',
+            map_clinique:'',
+            name_practician:'Guillaume',
+            img_practician:'',
+            speciality_practician:'Plastic surgery',
+            operations:['Breast surgery'],
+            description_clinic:'See big',
+            package:[''],
+        },
+        {
+            id:5,
+            clinic:'Smart HQ',
+            city:'Melbourn',
+            country:'Australia',
+            continent:'Oceania',
+            img_clinique:'https://www.lasource.ch/sites/default/files/styles/crop_3_2/public/images/content_types/page/specialites.jpg?itok=1eShRFbt',
+            map_clinique:'',
+            name_practician:'Trad',
+            img_practician:'',
+            speciality_practician:'Neurology',
+            operations:['Implant chips'],
+            description_clinic:"Welcome to the brain's headquarter!",
+            package:[''],
+        }
+    
+    ];
+        
+  
+     let template = document.getElementById('containPractician').innerHTML,
+     result = document.getElementById('containPractician'),
+     attachTemplateTopracticians;
+       
+   attachTemplateTopracticians = function (template, practicians) {
+     
+     let i = 0,
+       len = practicians.length,
+       fragment = "";
+ 
+     function replace(obj) {
+       let t, key, reg;
+ 
+       for (key in obj) {
+         reg = new RegExp("{" + key + "}", "ig");
+         t = (t || template).replace(reg, obj[key]);
+       }
+ 
+       return t;
+     }
+ 
+     for (; i < len; i++) {
+       fragment += replace(practicians[i]);
+     }
+ 
+     return fragment;
+   };
 
-];
+   result.innerHTML = attachTemplateTopracticians(template, practicians);
+        
+}
+
+
+    
+    
+    )();
